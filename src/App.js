6625df1932e7ef22e527c2a518e4e9dc42bbc876
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-06-18 21:18:01 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
+import Papa from 'papaparse';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1L7O902h4XCTaMP2iFCnIPy0j5XES3L2DbYmYB6KA8Bg/export?format=csv";
+const EVENT_FUNCTION_URL = "https://2xlqhuidw2k6n2ys7lb6lsnaje0mpymr.lambda-url.us-east-1.on.aws/";
 
 function App() {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const csvString = await fetch(SPREADSHEET_URL).then(e => e.text());
+      const results = Papa.parse(csvString, {header: true});
+      console.log(results);
+      setEvents(results.data);
+      setLoading(false);
+    })();
+  }, []);
+  const submitForm = async () => {
+    const data = {};
+    await fetch(EVENT_FUNCTION_URL, {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+  };
+  
+  if (loading) {
+    return "Loading...";
+  }
+  console.log(events);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Twitter username" />
+      <div style={{"height": 500, "overflow": "auto"}}>
+        {events.map(e => <div>
+          <input type="checkbox" />
+          Date: {e.date}<br />
+          Time: {e.time}<br />
+          ID: {e.id}<br />
+          Name: {e.name}<br />
+        </div>)}
+      </div>
+      <button onClick={submitForm}>Submit</button>
     </div>
   );
 }
