e9d48fb844334e467d1a8b2c01e8a697ad034bc2
Eva Zhang <evazhang@Evas-MBP.lan>
2022-06-19 18:20:18 -0400

    Adding input table form

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 const SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1L7O902h4XCTaMP2iFCnIPy0j5XES3L2DbYmYB6KA8Bg/export?format=csv";
-const EVENT_FUNCTION_URL = "https://2xlqhuidw2k6n2ys7lb6lsnaje0mpymr.lambda-url.us-east-1.on.aws/";
+const EVENT_FUNCTION_URL = "https://4fo54lv2w3.execute-api.us-east-1.amazonaws.com/default/events_fetch";
+
 
 function App() {
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
     (async () => {
       const csvString = await fetch(SPREADSHEET_URL).then(e => e.text());
@@ -19,33 +21,56 @@ function App() {
     })();
   }, []);
   const submitForm = async () => {
-    const data = {};
-    const results = await fetch(EVENT_FUNCTION_URL, {
+    
+    const data = {'name': 'testname'}; 
+
+  const results = await fetch(EVENT_FUNCTION_URL, {
       method: 'POST', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
-      },
       body: JSON.stringify(data),
-    }).then(e => e.json());
+    }).then(
+    function (response) {
+          console.log(response);
+          console.log(data.name);
+        },
+    );
     // do something with results
   };
+
+function displayTable(events){
+    let table = '<table border="1">';
+    table += '<tr><th>Going</th><th>Name</th><th>Date</th><th>Time</th></tr>'; 
+
+    events.forEach((e, index) => {
+    table = table + '<tr>';
+    table = table+ '<td> <input type="checkbox" /></td>';
+    table = table + '<td>${e.name}</td>';
+    table = table + '<td>${e.date}</td>';
+    table = table + '<td> ${e.time}</td>';
+    table += '</tr>';
+    });
+    table += "</table>";
+    document.getElementById("events-list").innerHTML = table;
+}
+
+  displayTable(events);
   
   if (loading) {
     return "Loading...";
   }
   console.log(events);
+
+
   return (
     <div className="App">
       <input type="text" placeholder="Twitter username" />
-      <div style={{"height": 500, "overflow": "auto"}}>
-        {events.map(e => <div>
-          <input type="checkbox" />
-          Date: {e.date}<br />
-          Time: {e.time}<br />
-          ID: {e.id}<br />
-          Name: {e.name}<br />
-        </div>)}
-      </div>
+      
+      // <div id="events-list">
+      // // displayTable(events);
+
+      // </div>
+
+
+
       <button onClick={submitForm}>Submit</button>
     </div>
   );
--- src/lambda_twitter_snippet.py
+++ src/lambda_twitter_snippet.py
@@ -1,114 +1,42 @@
-import boto3
-import base64
-from botocore.exceptions import ClientError
-
-
-def get_secret():
-
-    secret_name = "Demo/Twitter_API_Key"
-    region_name = "us-east-2"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-    else:
-        # Decrypts secret using the associated KMS key.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-        else:
-            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
-            
-    # Your code goes here. 
 # Use this code snippet in your app.
-# If you need more information about configurations or implementing the sample code, visit the AWS docs:   
-# https://aws.amazon.com/developers/getting-started/python/
-
 import boto3
-import base64
 from botocore.exceptions import ClientError
 
 
 def get_secret():
-
-    secret_name = "Demo/Twitter_API_Key"
+    secret_name = "Demo/Twitter_bearer_token"
+    endpoint_url = "https://secretsmanager.us-east-2.amazonaws.com"
     region_name = "us-east-2"
 
-    # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
-        region_name=region_name
+        region_name=region_name,
+        endpoint_url=endpoint_url
     )
-
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
 
     try:
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
     except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            print("The requested secret " + secret_name + " was not found")
         elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+            print("The request was invalid due to:", e)
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            print("The request had invalid params:", e)
     else:
-        # Decrypts secret using the associated KMS key.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        # Decrypted secret using the associated KMS CMK
+        # Depending on whether the secret was a string or binary, one of these fields will be populated
         if 'SecretString' in get_secret_value_response:
             secret = get_secret_value_response['SecretString']
         else:
-            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            binary_secret_data = get_secret_value_response['SecretBinary']
             
-    # Your code goes here. 
+        # (TODO): take this out of bearer token 
+    
+
+def get_twitter_friends(username):
+    
+    bearer_token = 'AAAAAAAAAAAAAAAAAAAAAFSodwEAAAAAxRbWSW8hrHsbs8IHAJacORB2XEU%3Dq0ZlNetbVoBc6qzgip7rjsIGhHsnDoP3dUBPDoKtXoteGoxeVH'
